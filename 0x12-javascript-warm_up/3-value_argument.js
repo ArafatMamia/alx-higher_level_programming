8571a789027c669b35ba192b9e1b521fcebc777e
Arafat Mamia <arafmamia@gmail.com>
2022-02-28 06:05:57 -0800

    argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-if(process.argv.length<=2){
-	console.log('No argument');
-}
-else if(process.argv.length == 3){
-	console.log(process.argv[2]);
+let args = process.argv.slice(2);
+if (args[0] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(args[0]);
 }
