3ecd499ecaf0079f8f9c0d0476ad4a3379288fb7
Arafat Mamia <arafmamia@gmail.com>
2022-02-28 06:36:53 -0800

    multi

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-let args = process.argv[2];
-if (isNaN(Number(args))) {
+'use strict';
+let x = process.argv[2];
+if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < Number(args); i++) {
+  for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
 }
