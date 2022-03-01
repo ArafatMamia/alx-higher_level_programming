9582d0fbd52e2aaca167b02a19470008a071224e
Arafat Mamia <arafmamia@gmail.com>
2022-03-01 02:42:39 -0800

    stars

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-class Rectangle{
+class Rectangle {
 	constructor(w, h){
 		if(w > 0 && h > 0){
 			this.width = w;
@@ -8,8 +8,8 @@ class Rectangle{
 		}
 
         }
-	print(){
-		for(let i = 0;i < this.height;i++){
+	print () {
+		for(let i = 0; i < this.height; i++){
                      console.log('X'.repeat(this.width));
 		}
 	}
