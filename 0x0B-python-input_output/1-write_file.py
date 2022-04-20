a0c403442ab4e8f77a240c14b9ba7e1d4a430549
Arafat <arafmamia@gmail.com>
2022-04-20 06:50:31 -0700

    write

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,6 @@ this module contain write_file function
 
 
 def write_file(filename="",text=""):
+    """ write_file function that write a text and return a number of characters"""
     with open(filename, "w", encoding="utf-8") as a_file:
         return a_file.write(text)
